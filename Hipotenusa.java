1789777ff79fda40dc464d15bb7d95e9ca31be38
guifernandess <gui_fernandes07@hotmail.com>
2017-03-19 22:18:16 -0300

    Hipotenusa

---
+++ Hipotenusa.java
@@ -0,0 +1,32 @@
+
+package hipotenusa;
+
+
+public class Hipotenusa {
+
+   
+    public static void main(String[] args) {
+      double cat;
+      cat = 3.8;
+      double cat2;
+      cat2 = 7.8;
+      double hipo;
+      hipo = (cat*cat)+(cat2*cat2);
+        System.out.println("VALOR DA HIPOTENUSA = " + hipo);
+        
+      
+              
+      
+      
+      
+      
+      
+              
+      
+             
+              
+              
+              
+    }
+    
+}
