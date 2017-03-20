d71bd6b5fbcc6e124af3667e1e212d5ee4e69fac
guifernandess <gui_fernandes07@hotmail.com>
2017-03-19 21:58:30 -0300

    Add files via upload
    Quilometragem por Litros

---
+++ QuilometragemsobreLitros.java
@@ -0,0 +1,19 @@
+package teste;
+
+
+public class QuilometragemsobreLitros {
+
+    public static void main(String[] args) {
+              // TODO code application logic here
+    
+    double distancia ;
+    distancia = 80.0;
+    double quantgas;
+    quantgas = 20.0;
+    double consumo;
+    consumo = (distancia/quantgas);
+    
+        System.out.println("valor do consumo "+consumo+ " km/litro");
+
+    }
+}
