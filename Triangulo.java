34962dd7d9af9b33c98395c20dd395dfc998ed47
guifernandess <gui_fernandes07@hotmail.com>
2017-03-19 22:12:54 -0300

    Perimetro

---
+++ Triangulo.java
@@ -0,0 +1,17 @@
+
+public class Triangulo {
+
+    public static void main(String[] args) {
+        double cat1;
+        cat1 = 2.0;
+        double cat2;
+        cat2 = 5.0;
+        double perimetro;
+        perimetro = (cat2 + cat1 );
+        System.out.println("O perimetro do triangulo retangulo é " + perimetro);
+
+    }
+
+}
+
+}
